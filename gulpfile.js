de4cda0d4200ece488f6719e218335b9a74c4863
nikbritoff <britov.nd@gmail.com>
2022-04-02 17:59:27 +0300

    Доработка сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
+const htmlmin = require('gulp-htmlmin');
+const csso = require("postcss-csso");
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 const del = require("del");
+const rename = require("gulp-rename");
+const replace = require('gulp-replace');
 
 // Clean
 
 const clean = () => del("build");
 
-// Copy 
+// Copy
 
 const copy = () => {
   return gulp.src([
@@ -19,11 +23,22 @@ const copy = () => {
     "src/img/**/*.{jpg,png,svg}",
   ], {
     base: "src"
-  })  
+  })
   .pipe(gulp.dest("build"))
 };
 
 exports.copy = copy;
+
+// HTML
+
+const htmlminify = () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(replace('css/style.css', 'css/style.min.css'))
+    .pipe(gulp.dest('build'));
+}
+
+exports.htmlminify = htmlminify;
 
 // Styles
 
@@ -41,6 +56,23 @@ const styles = () => {
 }
 
 exports.styles = styles;
+
+const buildStyles = () => {
+  return gulp.src("src/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(),
+      csso(),
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"));
+    // .pipe(sync.stream());
+}
+
+exports.buildStyles = buildStyles;
 
 // Server
 
@@ -74,3 +106,16 @@ exports.default = gulp.series(
     watcher,
   ),
 );
+
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  gulp.parallel(
+    buildStyles,
+    htmlminify,
+  )
+)
+
+exports.build = build;
